66b2c079977962d8bbef6a3b95951b7c3cb24c5e
Xtryker <github@mail.diavel.pt>
2026-09-09 20:16:09 +0100

    Update WazePT Pins.user.js

--- WazePT Pins.user.js
+++ WazePT Pins.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WazePT Pins
 // @namespace    https://greasyfork.org/en/users/1559074-xtryker
-// @version      6.30.2
+// @version      6.30.3
 // @description  Menu circular de clique direito para o Waze Map Editor: Marcar local, Copiar hiperligação permanente, Atualizar aqui, Lomba (Z), Semáforo (Shift+T), Estrada (I)
 // @author       Xtryker
 // @icon         https://i.imgur.com/UksVMzF.png
